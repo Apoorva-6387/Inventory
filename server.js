d126ca11b282877518df5533ccc019ecc43191ea
Apoorva-6387 <apoorva6387@gmail.com>
2025-07-25 09:32:12 +0530

    updates

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.json());
 
 app.use('/auth', require('./routes/auth'));
 app.use('/products', require('./routes/product'));
+app.get("/", (req, res) => {
+  res.send("Backend Working ✅");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
